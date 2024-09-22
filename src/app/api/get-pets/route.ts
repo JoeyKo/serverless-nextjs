0bf6643441f5931eeb34ee80b0a70c76e11c79d0
JoeyKo <joeyko1004@gmail.com>
2024-09-22 12:57:58 +0800

    feat: add get pets

---
+++ src/app/api/get-pets/route.ts
@@ -0,0 +1,11 @@
+import { sql } from '@vercel/postgres';
+import { NextResponse } from 'next/server';
+ 
+export async function GET() {
+  try {
+    const pets = await sql`SELECT * FROM Pets;`;
+    return NextResponse.json({ pets: pets.rows }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
